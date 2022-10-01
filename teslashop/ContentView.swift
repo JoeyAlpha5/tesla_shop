c27c88180518e1fecf8448195485d60e1bbf379f
Jalome <chjalome@gmail.com>
2022-10-01 14:51:52 +0200

    Initial Commit

---
+++ teslashop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  teslashop
+//
+//  Created by Jalome Chirwa on 2022/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ teslashop/teslashopApp.swift
@@ -0,0 +1,17 @@
+//
+//  teslashopApp.swift
+//  teslashop
+//
+//  Created by Jalome Chirwa on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct teslashopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
